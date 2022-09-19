4d1aa13757f249630cee6a36b252a42041dc13d1
Awet Tsegazeab <avvvet@gmail.com>
2022-09-19 12:50:23 +0300

    add util functions

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,16 +1,46 @@
 package util
 
 import (
+	"bytes"
 	"crypto/ecdsa"
+	"crypto/rand"
 	"crypto/x509"
+	"io"
+	"math/big"
 )
 
-func encode(publicKey *ecdsa.PublicKey) []byte {
+func StreamToByte(stream io.Reader) []byte {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.Bytes()
+}
+
+func StreamToString(stream io.Reader) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.String()
+}
+
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret), nil
+}
+
+func Encode(publicKey *ecdsa.PublicKey) []byte {
 	encodedByte, _ := x509.MarshalPKIXPublicKey(publicKey)
 	return encodedByte
 }
 
-func decode(encodedPub []byte) *ecdsa.PublicKey {
+func Decode(encodedPub []byte) *ecdsa.PublicKey {
 	genericPublicKey, _ := x509.ParsePKIXPublicKey(encodedPub)
 	publicKey := genericPublicKey.(*ecdsa.PublicKey)
 
